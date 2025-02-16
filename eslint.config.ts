8f697281bf8530b4919080f36f02ee2fc3b75b9a
John Lee <jlee1201@users.noreply.github.com>
2025-02-16 00:36:17 -0500

    remove esModuleInterop

---
+++ eslint.config.ts
@@ -0,0 +1,50 @@
+import type { Linter } from 'eslint';
+import * as eslint from '@eslint/js';
+import * as tseslint from '@typescript-eslint/eslint-plugin';
+import * as tsParser from '@typescript-eslint/parser';
+import * as reactPlugin from 'eslint-plugin-react';
+import * as prettierPlugin from 'eslint-plugin-prettier';
+import * as importPlugin from 'eslint-plugin-import';
+
+const config = [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser.default,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'react': reactPlugin,
+      'prettier': prettierPlugin,
+      'import': importPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error',
+      'import/no-default-export': 'error',
+      'import/prefer-default-export': 'off',
+      ...tseslint.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+      jest: true,
+    },
+  },
+] as const;
+
+export default config; 
